1e37aa214e41a071c91899cdcf755e99aeefefda
KEIICHI509186 <162006927+KEIICHI509186@users.noreply.github.com>
2024-11-04 20:20:10 +0900

    Add files via upload

---
+++ ②/①/script.js
@@ -0,0 +1,9 @@
+function Gif({ id }) {
+  const src = `https://media.giphy.com/media/${id}/giphy.gif`;
+  return <img src={src} alt="" />;
+}
+
+const app = <Gif id="33OrjzUFwkwEg" />;
+
+const root = document.getElementById('root');
+ReactDOM.render(app, root);
